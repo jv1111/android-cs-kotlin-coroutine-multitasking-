9b2701e4b161fb8d9dd3399b8ee9dee60b74d35f
jv1111 <webd0829@gmail.com>
2023-06-30 17:34:38 +0800

    update ui

--- app/src/main/java/com/example/d4_kotlin_coroutines_multitasking/MainActivity.kt
+++ app/src/main/java/com/example/d4_kotlin_coroutines_multitasking/MainActivity.kt
@@ -7,15 +7,19 @@ import android.widget.Button
 import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var tvMessage: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.tvCount)
+        tvMessage = findViewById(R.id.tvMessage)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
 
@@ -28,9 +32,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-    private fun downloadUserData() {
+    private suspend fun downloadUserData() {
         for (i in 1..200000) {
             Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                tvMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
+            delay(100);
         }
     }
 }
